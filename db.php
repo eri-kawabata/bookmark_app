6ef2918adf7feeee45bc35372bc331808288b982
eri-kawabata <eri.092127.k@gmail.com>
2024-12-20 01:11:04 +0900

    コメント

--- db.php
+++ db.php
@@ -1,9 +1,8 @@
 <?php
-$host = 'localhost';
-$dbname = 'bookmark_app';
-$user = 'root'; // XAMPPやMAMPのデフォルトユーザー名
-$pass = ''; // パスワード（通常は空）
-
+$host = 'localhost';  //データベース名
+$dbname = 'bookmark_app';  //DBホスト
+$user = 'root'; // サクラサーバーユーザー名
+$pass = ''; // パスワード
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
